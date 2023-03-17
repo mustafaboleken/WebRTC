3f345996bb31c54513406a7fb0ca639d4998e818
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-03-17 17:08:19 +0300

    Update package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "iOSWebRTCSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "iOSWebRTCSDK",
+	    targets: ["iOSWebRTCSDK"])
+        ],
+        dependencies: [ ],
+        targets: [
+            .target(
+                name: "WebRTC",
+                path: "WebRTC.xcframework"),
+	    .target(
+		name: "WebRTCiOSSDK"
+		path: "WebRTCiOSSDK.xcframework")
+    ]
+)

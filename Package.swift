fe73404bf3cd7ba1fe5aafb3e17d9e352241f6bf
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-03-17 18:27:00 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,9 +9,12 @@ let package = Package(
             name: "WebRTC",
 	    targets: ["WebRTC"]),
         ],
+	dependencies: [ ],
         targets: [
             .binaryTarget(
                 name: "WebRTC",
-                path: "https://github.com/stasel/WebRTC/releases/download/111.0.0/WebRTC-M111.xcframework.zip")
+                path: "https://github.com/stasel/WebRTC/releases/download/111.0.0/WebRTC-M111.xcframework.zip",
+            checksum: "b6b1b7924e539ae32286654509cc4e96465de399274d4a63bf0d2faae6f3a85b"
+	    ),
     ]
 )
